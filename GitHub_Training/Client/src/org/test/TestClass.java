d7219a316b9ae5bb25224c647e913afcdec7c00e
omarezzeldin <o.ezzeldin@ebeshara.com.eg>
2022-11-08 11:39:16 +0200

    commit message

---
+++ GitHub_Training/Client/src/org/test/TestClass.java
@@ -0,0 +1,14 @@
+package org.test;
+
+
+public class TestClass {
+    public TestClass() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello main");
+        System.out.println("Hello feature-01 beta");
+        System.out.println("Hello feature-02");
+    }
+}
